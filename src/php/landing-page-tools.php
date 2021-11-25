cbdfc901cd1a6c7f818eaab1b661b6567ec3434b
Rodrigo Gomes <digoartmusic@outlook.com>
2021-11-24 23:40:56 -0300

    EP-04 Include files

--- src/php/landing-page-tools.php
+++ src/php/landing-page-tools.php
@@ -23,60 +23,7 @@
 
 define('LPT_PLUGIN_FILE', __FILE__);
 
-final class Plugin
-{
 
-	private $version = "0.1.0";
-
-
-	private static $_instance = null;
-
-	/**
-	 * The Singleton's constructor should always be private to prevent direct
-	 * construction calls with the `new` operator.
-	 */
-	protected function __construct() { }
-
-	/**
-	 * Singletons should not be cloneable.
-	 */
-	protected function __clone() { }
-
-	/**
-	 * Singletons should not be restorable from strings.
-	 */
-	protected function __wakeup() { }
-
-	public static function getInstance() : ?Plugin
-	{
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
-	}
-
-	public function checkInstance()
-	{
-		echo "Este é o Objeto " , spl_object_id($this) , " da classe plugin versão ", $this->getVersion() , "\n" ;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getVersion(): string
-	{
-		return $this->version;
-	}
-
-	/**
-	 * @param string $version
-	 */
-	public function setVersion(string $version): void
-	{
-		$this->version = $version;
-	}
-
-}
 
 
 if (class_exists('Plugin')){
